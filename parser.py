8963e4dcab4d1751dc98bbdd1b96f0316c62e270
Егор <egor@MacBook-Air-Egor.local>
2022-08-26 17:20:56 +0300

    addeв new git rep

---
+++ parser.py
@@ -0,0 +1,64 @@
+from sre_constants import MAGIC
+import requests
+import time
+import re
+from bs4 import BeautifulSoup
+
+url = 'https://dead-god.ru'
+headers = {
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.6.1 Safari/605.1.15"
+}
+
+def get_desc(name_input):
+    try:
+        r = requests.get(url, headers=headers)
+        soup = BeautifulSoup(r.text, 'lxml')
+        Item_Name = soup.find('span', {'data-name': True}, string = re.compile(name_input, re.I))
+        Parent = Item_Name.parent
+        Item_Description = Parent.find('span', {'data-description' : True}).get_text() 
+        Item_Synergies = Parent.find('span', {'data-synergies' : True})
+        Item_Book_of_Virtues = Parent.find('span', {'book-of-virtues-wisp' : True})
+        Item_Judas_Birth_Right = Parent.find('span', {'judas-birthright-effect' : True})
+        Item_Bugs = Parent.find('span', {'data-bugs' : True})
+        if Item_Synergies != None:
+            Item_Synergies = Item_Synergies.text
+        else:
+            Item_Synergies = ''
+
+        if Item_Book_of_Virtues != None:
+            Item_Book_of_Virtues = Item_Book_of_Virtues.text
+        else:
+            Item_Book_of_Virtues = ''
+
+        if Item_Judas_Birth_Right != None:
+            Item_Judas_Birth_Right = Item_Judas_Birth_Right.text
+        else:
+            Item_Judas_Birth_Right = ''
+
+        if Item_Bugs != None:
+            Item_Bugs = Item_Bugs.text
+        else:
+            Item_Bugs = ''
+        return Item_Name.text, Item_Description, Item_Synergies, Item_Book_of_Virtues, Item_Judas_Birth_Right, Item_Bugs
+    except:
+        return "None, Pls try again", 'Ой! Что-то пошло не так, попробуйте ввести точное название предмета', '', '', '', ''
+
+
+def download_pics():
+    urls = []
+    names =[]
+    r = requests.get(url, headers=headers)
+    soup = BeautifulSoup(r.text, 'lxml')
+    for link in soup.find_all('span', {'data-icon' : True}):
+        urls.append(''.join(link.find_all(text=True)))
+    for name in soup.find_all('span', {'data-name' : True}):
+        names.append(''.join(name.find_all(text=True)))
+    for i in range(0, len(urls)):
+        url1 = urls[i]
+        img_data = requests.get(url1, verify=False).content
+        with open('downloads/' + names[i] + '.png', 'wb') as handler:
+            handler.write(img_data)
+    print('Done!')
+
+if __name__ == '__main__':
+    get_desc('The Bible')
---
+++ tg_bot.py
@@ -0,0 +1,79 @@
+from dotenv import load_dotenv, find_dotenv
+from aiogram import Bot, Dispatcher, types, executor
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+import os
+import parser
+
+#Get Token
+load_dotenv('/Users/egor/Documents/Python/TOKENS/.env')
+bot = Bot(os.getenv('TOKEN'))
+
+dp = Dispatcher(bot)
+
+Item_Syn1 = ''
+Item_Virtues1 = ''
+Item_Judas_BR1 = ''
+Item_Bugs1 = ''
+
+Message_Data = []
+
+@dp.message_handler(commands = ['start', 'help'])
+async def help(message: types.message):
+    await message.answer('Привет! Введи название предмета, а я покажу его описание!')
+
+#Get Item Description
+@dp.message_handler()
+async def get_description(message: types.message):
+    global Item_Syn1, Message_Data, Item_Virtues1, Item_Judas_BR1, Item_Bugs1
+    Item_Name, Item_Desc, Item_Syn, Item_Virtues, Item_Judas_BR, Item_Bugs = parser.get_desc(message.text)
+    Item_Syn1 = Item_Syn
+    Item_Virtues1 = Item_Virtues
+    Item_Judas_BR1 = Item_Judas_BR
+    Item_Bugs1 = Item_Bugs
+    if Item_Name != 'None, Pls try again':
+        with open('downloads/'+ Item_Name +'.png', 'rb') as f:
+            Image = f.read()
+            if Item_Syn !='':
+                Main_Menu.insert(Learn_More_Button)
+            if Item_Virtues != '':
+                Main_Menu.insert(Book_of_Virtues_Button)
+            if Item_Judas_BR != '':
+                Main_Menu.insert(Judas_BR_Button)
+            if Item_Bugs != '':
+                Main_Menu.insert(Bugs_Button)
+            await bot.send_photo(message.from_user.id, Image, Item_Desc, reply_markup=Main_Menu)
+            Main_Menu.inline_keyboard.clear()
+            Item_Name = ''
+            Item_Syn = ''
+            Item_Virtues = ''
+            Item_Judas_BR = ''
+            Item_Bugs = ''
+    else:
+        await message.answer(Item_Desc)
+
+
+# InLine Buttons
+Main_Menu = InlineKeyboardMarkup(row_width=1)
+Learn_More_Button = InlineKeyboardButton('Узнать больше', callback_data='Learn_More_Button')
+Book_of_Virtues_Button = InlineKeyboardButton('Книга добродетелей', callback_data='Book_of_Virtues_Button')
+Judas_BR_Button = InlineKeyboardButton('Право Первородства Иуды', callback_data='Judas_BR_Button')
+Bugs_Button = InlineKeyboardButton('Баги', callback_data='Bugs_Button')
+
+@dp.callback_query_handler(text='Learn_More_Button')
+async def Learn_More(message: types.message):
+    await bot.send_message(message.from_user.id, Item_Syn1.replace('item:[','').replace(']',''))
+
+@dp.callback_query_handler(text='Book_of_Virtues_Button')
+async def Book_of_Virtues(message: types.message):
+    await bot.send_message(message.from_user.id, Item_Virtues1.replace('item:[','').replace(']',''))
+
+@dp.callback_query_handler(text='Judas_BR_Button')
+async def Learn_More(message: types.message):
+    await bot.send_message(message.from_user.id, Item_Judas_BR1.replace('item:[','').replace(']',''))
+
+@dp.callback_query_handler(text='Bugs_Button')
+async def Learn_More(message: types.message):
+    await bot.send_message(message.from_user.id, Item_Bugs1.replace('item:[','').replace(']',''))
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
